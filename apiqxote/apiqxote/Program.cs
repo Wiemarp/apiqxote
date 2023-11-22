8eb7061f4749e95c9e06cfb227c5f70642b6368f
scalis <svencalis2004@gmail.com>
2023-11-22 16:26:51 +0100

    database scaffold mysql

--- apiqxote/apiqxote/Program.cs
+++ apiqxote/apiqxote/Program.cs
@@ -1,4 +1,8 @@
 
+using apiqxote.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 namespace apiqxote
 {
     public class Program
@@ -14,19 +18,38 @@ namespace apiqxote
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo() { Title = "API Docs", Version = "v1" });
+            });
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("QxoteConnectionString"));
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(config =>
+                {
+                    config.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                });
             }
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseCors(options =>
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyOrigin();
+                options.AllowAnyMethod();
+            });
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
